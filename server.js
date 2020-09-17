5b3547b0d47d761ec54839da39fd4715053201d0
Dima Matroskin <dima.matroskin062@gmail.com>
2020-09-17 23:05:54 +0300

    one more deploy try

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ app.use(cors());
 
 app.use('/graphiql', graphqlHTTP({ schema, graphiql: true }));
 
-app.use(express.static('/client/build'));
+app.use(express.static('./client/build'));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '/client/build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, './client/build'));
 });
 
 app.listen(PORT, (err) =>
