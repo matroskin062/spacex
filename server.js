8f5200b9906aa0d8073f7b2c69f0ab814985bfe0
Dima Matroskin <dima.matroskin062@gmail.com>
2020-09-17 22:38:21 +0300

    deploy try

--- server.js
+++ server.js
@@ -2,13 +2,20 @@ const express = require('express');
 const cors = require('cors');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema');
-
 const app = express();
+const path = require('path');
 
+const PORT = process.env.PORT || 3001;
 app.use(cors());
 
 app.use('/', graphqlHTTP({ schema, graphiql: true }));
 
-app.listen(3001, (err) =>
+app.use(express.static('client/build'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
+});
+
+app.listen(PORT, (err) =>
   err ? console.log(err) : console.log('GraphQL server is running!!!')
 );
